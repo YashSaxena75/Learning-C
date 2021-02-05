6a7bfd08b9fcdf15fa4aa3d259c6b79d14ab2763
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2021-02-05 09:43:55 +0530

    HackerRank 30 Days

---
+++ hack.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+// =============================HackerRank 30 Days=================================
+
+void day0()
+{
+    // Declare a variable named 'input_string' to hold our input.
+    char input_string[105]; 
+    
+    // Read a full line of input from stdin and save it to our variable, input_string.
+    scanf("%[^\n]", input_string); 
+    
+    // Print a string literal saying "Hello, World." to stdout using printf.
+    printf("Hello, World.\n");
+    
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    printf("%s",input_string);
+    return 0;
+}
+
+void day1()
+{
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+    int j ;
+    double e =0;
+    char st[105];
+    
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d",&j);
+    scanf("%lf",&e);
+    scanf("\n");
+    scanf("%[^\n]%*c", st); 
+    // Print the sum of both integer variables on a new line.
+    printf("%d\n",i+j);
+    printf("%.1lf\n",d+e);
+    printf("%s%s",s,st);
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+}
+
+int main()
+{
+    //day0();
+    day1();
+}
