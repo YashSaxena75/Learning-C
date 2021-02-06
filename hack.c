4c1a058fde1675dbac12cf8d367de00ce52fe7d2
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2021-02-06 12:31:03 +0530

    Update hack.c

--- hack.c
+++ hack.c
@@ -74,11 +74,32 @@ void day3()
     printf("Weird");
     }
 }
+void day4()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if(a>b && a>c && a>d)
+    {
+        printf("%d",a);
+    }
+    else if(b>a && b>c && b>d)
+    {
+        printf("%d",b);
+    }
+    else if(c>a && c>b && c>d)
+    {
+        printf("%d",c);
+    }
+    else {
+    printf("%d",d);
+    }
+}
 
 int main()
 {
     //day0();
     //day1();
     //day2();
-    day3();
+    //day3();
+    day4();
 }
